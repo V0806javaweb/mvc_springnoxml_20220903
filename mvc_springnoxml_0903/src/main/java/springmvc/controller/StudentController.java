23eef73ea7eaccca6466df3970ed60fe393b99bb
student <student@DESKTOP-4ARRLB2>
2022-09-03 16:33:52 +0800

    student noXML version

---
+++ mvc_springnoxml_0903/src/main/java/springmvc/controller/StudentController.java
@@ -0,0 +1,30 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.ModelMap;
+
+import demo.model.Student;
+
+@Controller
+public class StudentController {
+
+@RequestMapping(value = "/student" , method = RequestMethod.GET)
+	public ModelAndView student(@ModelAttribute("student") Student student)
+	{ 
+	     Student s=new Student(20,"Mary",7);
+	     return new ModelAndView("student", "command", s); 
+	}
+
+	@RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+	public String addStudent(@ModelAttribute("student") Student student, ModelMap model) {
+		model.addAttribute("name", student.getName());
+		model.addAttribute("age", student.getAge());
+		model.addAttribute("id", student.getId());
+
+		return "result";
+	}
+}
